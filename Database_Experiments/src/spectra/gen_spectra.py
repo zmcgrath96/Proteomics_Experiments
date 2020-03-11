c85e7a470bc17000cc9b4e321390a11f5e5b63c6
Zach <zmcgrath96@gmail.com>
2020-03-11 11:16:47 -0600

    separated b and y ion mass calculation

--- Database_Experiments/src/spectra/gen_spectra.py
+++ Database_Experiments/src/spectra/gen_spectra.py
@@ -22,17 +22,81 @@ amino_acids={
     "V":99.068414
 }
 
-'''calc_masses
+def __b_ions(sequence: str, charge=None): 
+    '''
+    Calculate the masses for b_ions
+    
+    Inputs:
+        sequence:  string amino acid sequence to calculate
+    kwargs:
+        charge:    int charge to calculate. Possible types are {1, 2}. Default is both
+    Outputs:
+        list of floats
+    '''
+    masses = []
+    length = len(sequence)
+    
+    if charge is None or charge == 1:
+        #b+
+        total = 1.007825035 - 0.0005486 #for the H to turn the residue NH on the N-terminus into NH2
+        for i in range (0, length):
+            total += amino_acids[sequence[i]]
+            masses.append(total)
+            #Since z (the charge) is equal to one, the total here is the m/z
+            
+    if charge is None or charge == 2:
+        #b++
+        total = 2 * 1.007825035 - 2 * 0.0005486 #adding one more proton this time to make it doubly charged
+        for i in range (0, length):
+            total += amino_acids[sequence[i]]
+            masses.append(total/2)
+            
+    return masses
 
-DESC:
-    calculates the masses/spectrum for a sequence
-Inputs:
-    sequence: str amino acid sequence to change to list of masses
-    charge: int charge value to calculate masses for
-Outputs:
-    list of floats, float       spectrum and the precursor mass 
-'''
-def calc_masses(sequence, charge):
+def __y_ions(sequence: str, charge=None): 
+    '''
+    Calculate the masses for y_ions
+    
+    Inputs:
+        sequence:  string amino acid sequence to calculate
+    kwargs:
+        charge:    int charge to calculate. Possible types are {1, 2}. Default is both
+    Outputs:
+        list of floats
+    '''
+    masses = []
+    length = len(sequence)
+    
+    if charge is None or charge == 1:
+        #y+
+        total = 3 * 1.007825035 + 15.99491463 - 0.0005486 #for the OH to turn the residue CO on the C-terminus into COOH + 1 proton to make NH into NH2 and 1 proton make positively charged
+        for i in range (0,length):
+            total += amino_acids[sequence[length-i-1]]
+            masses.append(total)
+            
+    if charge is None or charge == 2:
+        #y++
+        total = 4 * 1.007825035 + 15.99491463 - 2 * 0.0005486 #another proton to make doubly charged
+        for i in range (0, length):
+            total += amino_acids[sequence[length-i-1]]
+            masses.append(total/2)
+            
+    return masses
+
+def calc_masses(sequence: str, charge=None, ion=None) -> (list, float):
+    '''
+    Calculate the molecular weight (Da) of an Amino Acid sequence
+    
+    Inputs:
+        sequence:   string amino acid sequence to calculate mass of
+    kwargs:
+        charge:     int charge to calculate. Possible values are {1, 2}. Default is both
+        ion:        string ion type to calculate. Possible values are {'b', 'y'}. Default is both
+    Output:
+        (masses, precursor_mass)
+        masses:         list of floats of spectrum calculated
+        precursor_mass: float precursor mass of the entire amino acid sequence
+    '''
     masses = []
 
     length = len(sequence)
@@ -40,57 +104,51 @@ def calc_masses(sequence, charge):
     for i in range(length):
         total +=  amino_acids[sequence[i]]
 
-    pre_mz = (total+charge*1.0072764)/charge   
-
-    if charge == 1:
-        #b+
-        total = 1.007825035 - 0.0005486 #for the H to turn the residue NH on the N-terminus into NH2
-        for i in range (0, length):
-            total += amino_acids[sequence[i]]
-            masses.append(total)
-            #Since z (the charge) is equal to one, the total here is the m/z
-
-        #y+
-        total = 3 * 1.007825035 + 15.99491463 - 0.0005486 #for the OH to turn the residue CO on the C-terminus into COOH + 1 proton to make NH into NH2 and 1 proton make positively charged
-        for i in range (0,length):
-            total += amino_acids[sequence[length-i-1]]
-            masses.append(total)
-
-    elif charge == 2:
-        #b++
-        total = 2 * 1.007825035 - 2 * 0.0005486 #adding one more proton this time to make it doubly charged
-        for i in range (0, length):
-            total += amino_acids[sequence[i]]
-            masses.append(total/2)
-
-        #y++
-        total = 4 * 1.007825035 + 15.99491463 - 2 * 0.0005486 #another proton to make doubly charged
-        for i in range (0, length):
-            total += amino_acids[sequence[length-i-1]]
-            masses.append(total/2)
-        #The masses you get exactly match Spectrum Mill. To get this, I had to make sure to use the mass of H+ and the mass of H when appropriate.
-
+    pre_mz_charge = 2 if charge is None else charge
+    pre_mz = (total+pre_mz_charge*1.0072764)/pre_mz_charge   
+    
+    if ion is None or ion == 'b': 
+        masses += __b_ions(sequence, charge=charge)
+        
+    if ion is None or ion == 'y': 
+        masses += __y_ions(sequence, charge=charge)
+        
     return masses, pre_mz
 
-'''gen_spectra
+def gen_spectrum(sequence: str, charge=None, ion=None) -> list:
+    '''
+    Generate a spectrum for a single sequence. Includes singly and doubly charged masses
+    
+    Inputs:
+        sequence: string amino acid sequence to calculate spectra for
+    kwargs:
+        charge:   int charge value to calculate masses for. Possible types are {1, 2}. Default is both
+        ion:      string ion type to calculate masses for. Possible types are {'b', 'y'}. Default is both
+    Outputs:
+        dictionary with the following values 
+        {
+            'spectrum': list of floats,
+            'precursor_mass': float,
+        }
+    '''
+    
+    this_entry = {}
+    masses, pre_mz = calc_masses(sequence, charge=charge, ion=ion)
+    masses.sort()
+    this_entry['spectrum'] = masses
+    this_entry['precursor_mass'] = pre_mz
+    return this_entry
 
-DESC:
-    generates mass spectra for sequences
-Inputs:
-    sequences: list of strings sequences to generate spectra for
-Outputs:
-    list of dictionaries of the form {'spectrum': list of floats, 'precursor_mass': float}
-'''
-def gen_spectra(sequenences):
-    spectra = []
-    for sequence in sequenences:
-        this_entry = {}
-        mass_1, _ = calc_masses(sequence, 1)
-        mass_2, pre_mz = calc_masses(sequence, 2)
-        this_spectra = mass_1 + mass_2
-        this_spectra.sort()
-        this_entry['spectrum'] = this_spectra
-        this_entry['precursor_mass'] = pre_mz
-        spectra.append(this_entry)
+def gen_spectra(sequences: list, charge=None, ion=None) -> list:
+    '''
+    Generates mass spectra for a list of sequences. Includes singly and doubly charged masses
 
-    return spectra
+    Inputs:
+        sequences: list of strings sequences to generate spectra for
+    kwargs:
+        charge:   int charge value to calculate masses for. Possible types are {1, 2}. Default is both
+        ion:      string ion type to calculate masses for. Possible types are {'b', 'y'}. Default is both
+    Outputs:
+        list of dictionaries of the form {'spectrum': list of floats, 'precursor_mass': float}
+    '''
+    return [gen_spectrum(seq, charge=charge, ion=ion) for seq in sequences]
